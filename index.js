3c2a4c702291bb4dde04dfa80eae27325b30c074
tamisa23 <tamisa_2010@hotmail.com>
2025-02-27 23:50:31 +0700

    Fix: Serve index.html from Root Directory

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+// เสิร์ฟไฟล์ index.html จาก Root Directory
 app.get("/", (req, res) => {
-    res.send("Hello, NOVA!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
